a9d5879e814a0371cbafefd1ca767bf3239c3ff8
stiderspace <stiderspace@gmail.com>
2021-12-19 02:24:43 +0100

    version update

--- src/main/java/com/silvaniastudios/roads/FurenikusRoads.java
+++ src/main/java/com/silvaniastudios/roads/FurenikusRoads.java
@@ -38,7 +38,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class FurenikusRoads {
 	
 	public static final String MODID = "furenikusroads";
-	public static final String VERSION = "1.1.10";
+	public static final String VERSION = "1.2.0k";
 	
 	@Instance(MODID)
 	public static FurenikusRoads instance;
